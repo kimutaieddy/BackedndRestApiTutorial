a94a6e1433a6a7e781075f54fa6ca82f466bcdaa
Your Name <eddykimutai@outlook.com>
2024-09-12 23:34:46 +0300

    Refactor settings.py to add REST_FRAMEWORK pagination configuration

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
