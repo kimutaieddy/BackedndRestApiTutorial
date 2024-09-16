c32e16dea049a45ca8ee20371fefb302f03b57bd
Your Name <eddykimutai@outlook.com>
2024-09-16 23:47:36 +0300

    Refactor UserViewSet to include UserSerializer and fix import permissions order

--- DjangoRest/world/quickstart/views.py
+++ DjangoRest/world/quickstart/views.py
@@ -7,4 +7,10 @@ from rest_framework import import permisiions, viewsets
 from world.quickstart.serializers import UserSerializer, GroupSerializer
 
 
-class
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permisiions_classes = [permisiions.IsAuthenticated]
