504007e0002380a9a7a09f5f01ba36ec6b67eaeb
Your Name <eddykimutai@outlook.com>
2024-09-16 22:59:32 +0300

    Refactor UserSerializer to rename groupSerializer to GroupSerializer and include additional fields

--- DjangoRest/world/quickstart/serializers.py
+++ DjangoRest/world/quickstart/serializers.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import Group,User
 from rest_framework import serializers
 
+
+# The UserSerializer class is similar to the User model, except that it uses the User model and has a different set of fields.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+
+# The GroupSerializer class is similar to the UserSerializer class, except that it uses the Group model and has a different set of fields.
+class GroupSerializer(serializers.HyperlinkedModelSerializer): 
     class Meta:
         model = Group
         fields = ('url', 'name')
