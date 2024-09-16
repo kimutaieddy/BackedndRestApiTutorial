95ce64ce23c51b55e0eff0de3b853005fe7f2273
Your Name <eddykimutai@outlook.com>
2024-09-16 23:47:03 +0300

    Refactor UserSerializer to fix import permissions order and include additional fields

--- DjangoRest/world/quickstart/views.py
+++ DjangoRest/world/quickstart/views.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User, Group
 from rest_framework import import permisiions, viewsets
 
 from world.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class
