51b2978d9eabc16fc94c4d74bde1a750acb63012
Your Name <eddykimutai@outlook.com>
2024-09-17 11:01:07 +0300

    Refactor import statement in urls.py to fix typo in 'Groups' viewset registration

--- DjangoRest/world/world/urls.py
+++ DjangoRest/world/world/urls.py
@@ -20,10 +20,10 @@ from quickstart import views
 
 routers = routers.DefaultRouter()   #create a router object
 routers.register(r'users',views.UserViewSet)    #register the viewset with the router
-routers.register(r'groups',views.GroupViewSet)  #register the viewset with the router
+routers.register(r'Groups',views.GroupViewSet)  #register the viewset with the router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',admin.site.urls),
     path('',include(routers.urls)),  #include the router urls
     path('api-auth/',include('rest_framework.urls',namespace='rest_framework'))  #include the rest_framework urls
 ]
