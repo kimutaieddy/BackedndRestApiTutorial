aa7b66c486bb5d9ed24e4801a993670d17ec9569
Your Name <eddykimutai@outlook.com>
2024-09-17 10:54:54 +0300

    Refactor import statement in views.py to fix typo in 'permissions'

--- DjangoRest/world/quickstart/views.py
+++ DjangoRest/world/quickstart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User, Group
-from rest_framework import permisiions, viewsets
+from rest_framework import permissions, viewsets
 
 from world.quickstart.serializers import UserSerializer, GroupSerializer
 
